baf24dccaad49f9295aee7f93ad9ec6a84c33e07
Napster <napster@npstr.space>
2021-10-03 22:32:45 +0200

    Move object mapper code to common

--- postgres/src/main/kotlin/dev/capybaralabs/d4j/store/postgres/PostgresStoreLayout.kt
+++ postgres/src/main/kotlin/dev/capybaralabs/d4j/store/postgres/PostgresStoreLayout.kt
@@ -1,22 +1,8 @@
 package dev.capybaralabs.d4j.store.postgres
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY
-import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE
-import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_ONLY
-import com.fasterxml.jackson.annotation.JsonInclude.Include.ALWAYS
-import com.fasterxml.jackson.annotation.JsonInclude.Include.CUSTOM
-import com.fasterxml.jackson.annotation.JsonInclude.Value
-import com.fasterxml.jackson.annotation.PropertyAccessor.ALL
-import com.fasterxml.jackson.annotation.PropertyAccessor.CREATOR
-import com.fasterxml.jackson.annotation.PropertyAccessor.GETTER
-import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.databind.DeserializationContext
-import com.fasterxml.jackson.databind.JsonDeserializer
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.deser.DeserializationProblemHandler
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import dev.capybaralabs.d4j.store.common.CommonDataAccessor
 import dev.capybaralabs.d4j.store.common.CommonGatewayDataUpdater
+import dev.capybaralabs.d4j.store.common.Serde
 import dev.capybaralabs.d4j.store.postgres.repository.PostgresChannelRepository
 import dev.capybaralabs.d4j.store.postgres.repository.PostgresEmojiRepository
 import dev.capybaralabs.d4j.store.postgres.repository.PostgresGuildRepository
@@ -30,11 +16,7 @@ import dev.capybaralabs.d4j.store.postgres.repository.PostgresVoiceStateReposito
 import discord4j.common.store.api.layout.DataAccessor
 import discord4j.common.store.api.layout.GatewayDataUpdater
 import discord4j.common.store.api.layout.StoreLayout
-import discord4j.discordjson.possible.PossibleFilter
-import discord4j.discordjson.possible.PossibleModule
 import io.r2dbc.spi.ConnectionFactory
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 
 /**
  * TODO
@@ -49,12 +31,7 @@ import org.slf4j.LoggerFactory
  */
 class PostgresStoreLayout(connectionFactory: ConnectionFactory) : StoreLayout {
 
-
-	companion object {
-		val log: Logger = LoggerFactory.getLogger(PostgresStoreLayout::class.java)
-	}
-
-	private val serde: PostgresSerde = JacksonJsonSerde(objectMapper())
+	private val serde: PostgresSerde = JacksonJsonSerde(Serde.objectMapper())
 
 	// TODO implement versioning / migrations
 	// TODO avoid blocking calls (db creations) in constructor?
@@ -82,30 +59,4 @@ class PostgresStoreLayout(connectionFactory: ConnectionFactory) : StoreLayout {
 		return postgresGatewayDataUpdater
 	}
 
-	// Copied over from RedisStoreDefaults
-	private fun objectMapper(): ObjectMapper {
-		return ObjectMapper()
-			.registerModule(PossibleModule())
-			.registerModule(Jdk8Module())
-			.setVisibility(ALL, NONE)
-			.setVisibility(GETTER, PUBLIC_ONLY)
-			.setVisibility(CREATOR, ANY)
-			.setDefaultPropertyInclusion(
-				Value.construct(
-					CUSTOM,
-					ALWAYS, PossibleFilter::class.java, null
-				)
-			)
-			.addHandler(object : DeserializationProblemHandler() {
-				override fun handleUnknownProperty(
-					ctxt: DeserializationContext, p: JsonParser,
-					deserializer: JsonDeserializer<*>, beanOrClass: Any,
-					propertyName: String,
-				): Boolean {
-					log.warn("Unknown property in {}: {}", beanOrClass, propertyName)
-					p.skipChildren()
-					return true
-				}
-			})
-	}
 }
