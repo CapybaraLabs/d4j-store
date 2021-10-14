b1633073e822c7b9ca3d696525f8b403c1c08c0a
Napster <napster@npstr.space>
2021-10-15 00:24:50 +0200

    Implement redis presence repo

--- redis/src/main/kotlin/dev/capybaralabs/d4j/store/redis/repository/RedisPresenceRepository.kt
+++ redis/src/main/kotlin/dev/capybaralabs/d4j/store/redis/repository/RedisPresenceRepository.kt
@@ -1,5 +1,6 @@
 package dev.capybaralabs.d4j.store.redis.repository
 
+import dev.capybaralabs.d4j.store.common.collectSet
 import dev.capybaralabs.d4j.store.common.repository.PresenceRepository
 import dev.capybaralabs.d4j.store.redis.RedisFactory
 import discord4j.discordjson.json.PresenceData
@@ -11,7 +12,13 @@ class RedisPresenceRepository(prefix: String, factory: RedisFactory) : RedisRepo
 	private val presenceKey = key("presence")
 	private val presenceOps = factory.createRedisHashOperations<String, String, PresenceData>()
 
-	private fun presenceKey(guildId: Long, userId: Long): String {
+	// a user can have "multiple" presences (due to sharding), so our indices have to handle that
+	// kinda dumb to have the guildId as part of the index values here BUT we need it because it is not unique
+	// we _could_ think about optimizing this
+	private val guildIndex = oneWayIndex("$presenceKey:guild-index", factory, String::class.java)
+	private val gShardIndex = twoWayIndex("$presenceKey:guild-shard-index", factory)
+
+	private fun presenceId(guildId: Long, userId: Long): String {
 		return "$guildId:$userId"
 	}
 
@@ -24,22 +31,58 @@ class RedisPresenceRepository(prefix: String, factory: RedisFactory) : RedisRepo
 			return Mono.empty()
 		}
 		return Mono.defer {
-			presenceOps.putAll(presenceKey, presences.associateBy { presenceKey(guildId, it.user().id().asLong()) }).then()
+			val presenceIds = presences.map { presenceId(guildId, it.user().id().asLong()) }
+			val addToGuildIndex = guildIndex.addElements(guildId, *presenceIds.toTypedArray())
+			val addGuildToShardIndex = gShardIndex.addElements(shardId, listOf(guildId))
+
+			val presencesById = presences.associateBy { presenceId(guildId, it.user().id().asLong()) }
+			val save = presenceOps.putAll(presenceKey, presencesById)
+
+			Mono.`when`(addToGuildIndex, addGuildToShardIndex, save)
 		}
 	}
 
 	override fun deleteById(guildId: Long, userId: Long): Mono<Long> {
 		return Mono.defer {
-			presenceOps.remove(presenceKey, presenceKey(guildId, userId))
+			val removeFromGuildIndex = guildIndex.removeElements(guildId, presenceId(guildId, userId))
+			val remove = presenceOps.remove(presenceKey, presenceId(guildId, userId))
+
+			Mono.`when`(removeFromGuildIndex)
+				.then(remove)
 		}
 	}
 
 	override fun deleteByGuildId(guildId: Long): Mono<Long> {
-		TODO("Not yet implemented")
+		return Mono.defer {
+			guildIndex.getElementsInGroup(guildId).collectList()
+				.flatMap { presenceIds ->
+					val deleteGuildIndexEntry = guildIndex.deleteGroup(guildId)
+					val removeGuildFromShardIndex = gShardIndex.removeElements(guildId)
+
+					val remove = presenceOps.remove(presenceKey, *presenceIds.toTypedArray())
+
+					Mono.`when`(deleteGuildIndexEntry, removeGuildFromShardIndex)
+						.then(remove)
+				}
+		}
 	}
 
 	override fun deleteByShardId(shardId: Int): Mono<Long> {
-		TODO("Not yet implemented")
+		return Mono.defer {
+			gShardIndex.getElementsByGroup(shardId).collectSet()
+				.flatMap { guildIds ->
+					val fetchPresenceIds = guildIndex.getElementsInGroups(guildIds).collectList()
+
+					val removeGuildsFromShardIndex = gShardIndex.deleteByGroupId(shardId)
+					val deleteGuildsFromGuildIndex = guildIndex.deleteGroups(guildIds)
+
+					fetchPresenceIds.flatMap { presenceIds ->
+						Mono.`when`(removeGuildsFromShardIndex, deleteGuildsFromGuildIndex).then(Mono.just(presenceIds))
+					}
+				}
+				.flatMap { presenceIds -> presenceOps.remove(presenceKey, *presenceIds.toTypedArray()) }
+		}
+
 	}
 
 	override fun countPresences(): Mono<Long> {
@@ -49,7 +92,9 @@ class RedisPresenceRepository(prefix: String, factory: RedisFactory) : RedisRepo
 	}
 
 	override fun countPresencesInGuild(guildId: Long): Mono<Long> {
-		TODO("Not yet implemented")
+		return Mono.defer {
+			guildIndex.countElementsInGroup(guildId)
+		}
 	}
 
 	override fun getPresences(): Flux<PresenceData> {
@@ -59,12 +104,16 @@ class RedisPresenceRepository(prefix: String, factory: RedisFactory) : RedisRepo
 	}
 
 	override fun getPresencesInGuild(guildId: Long): Flux<PresenceData> {
-		TODO("Not yet implemented")
+		return Flux.defer {
+			guildIndex.getElementsInGroup(guildId).collectList()
+				.flatMap { presenceOps.multiGet(presenceKey, it) }
+				.flatMapMany { Flux.fromIterable(it) }
+		}
 	}
 
 	override fun getPresenceById(guildId: Long, userId: Long): Mono<PresenceData> {
 		return Mono.defer {
-			presenceOps.get(presenceKey, presenceKey(guildId, userId))
+			presenceOps.get(presenceKey, presenceId(guildId, userId))
 		}
 	}
 }
